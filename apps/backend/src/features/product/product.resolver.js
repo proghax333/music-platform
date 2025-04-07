924f6740f57a2ac6f5f8214337ecd5ec7a30e828
proghax333 <skillzworldtech@gmail.com>
2025-04-07 16:50:40 +0530

    fix: product posting feature

--- apps/backend/src/features/product/product.resolver.js
+++ apps/backend/src/features/product/product.resolver.js
@@ -1,115 +1,124 @@
+import createHttpError from "http-errors";
 import { resolver } from "../../lib/graphql.js";
 
-export class ProductResolver{
-    /** @type {import("mongoose").Model} */
-    Product;
-    /** @type {import("mongoose").Model} */
-    ProductVariant;
-    /** @type {import("mongoose").Model} */
-    ProductPosting;
-    /** @type {import("mongoose").Model} */
-    Profile;
-    /** @type {import("mongoose").Model} */
-    User;
-    
-    constructor(Product,ProductVariant,ProductPosting , Profile, User){
-        this.Product = Product;
-        this.ProductVariant= ProductVariant;
-        this.ProductPosting = ProductPosting;
-        this.Profile = Profile;
-        this.User = User;
-    }
+export class ProductResolver {
+  /** @type {import("mongoose").Model} */
+  Product;
+  /** @type {import("mongoose").Model} */
+  ProductVariant;
+  /** @type {import("mongoose").Model} */
+  ProductPosting;
+  /** @type {import("mongoose").Model} */
+  Profile;
+  /** @type {import("mongoose").Model} */
+  User;
 
-    static get deps() {
-        return ["Product", "ProductVariant", "ProductPosting", "Profile", "User"];
-    }
+  constructor(Product, ProductVariant, ProductPosting, Profile, User) {
+    this.Product = Product;
+    this.ProductVariant = ProductVariant;
+    this.ProductPosting = ProductPosting;
+    this.Profile = Profile;
+    this.User = User;
+  }
 
-    createProduct = resolver(async (parent, args, context, info) => {
-        const {
-            name,
-            description,
-            images,
-            sku,
-            features,
-            price,
+  static get deps() {
+    return ["Product", "ProductVariant", "ProductPosting", "Profile", "User"];
+  }
 
-            variants
-        } = args.input;
+  createProduct = resolver(async (parent, args, context, info) => {
+    const {
+      name,
+      description,
+      images,
+      sku,
+      features,
+      price,
 
-        console.log(args.input);
+      variants,
+    } = args.input;
 
-        const product = await this.Product.create({
-            name,
-            description,
-            images,
-            sku,
-            features,
-            price,
-        });
-        
-        for(const variant of variants) {
-            const result = await this.ProductVariant.create({
-                ...variant,
-                product: product._id
-            });
-        }
+    console.log(args.input);
 
-        console.log(product);
-
-        const productObject = await product.tedoObject();
-
-
-        return {
-            message: "Product created successfully",
-            product: productObject,
-        }
+    const product = await this.Product.create({
+      name,
+      description,
+      images,
+      sku,
+      features,
+      price,
     });
 
-    _ProductVariant = async (parent, args, context) => {
-        const variants = await this.ProductVariant.find({
-            product: parent._id
-        });
-
-        return variants.map(x => x.toObject());
+    for (const variant of variants) {
+      const result = await this.ProductVariant.create({
+        ...variant,
+        product: product._id,
+      });
     }
 
-    createProductPosting = resolver(async (parent, args, context , info)=>{
-        const{
-            variant,
-            seller,
-            price
-        } = args.input;
+    console.log(product);
 
-        const productPosting = await this.ProductPosting.create({
-            variant,
-            seller,
-            price
-        });
+    const productObject = await product.toObject();
 
-        const productPostingObject = await productPosting.tedoObject();
+    return {
+      message: "Product created successfully",
+      product: productObject,
+    };
+  });
 
-        return{
-            message: "Product has been posted",
-            productPosting: productPostingObject
-        }
-    })
+  Product_variants = async (parent, args, context) => {
+    const variants = await this.ProductVariant.find({
+      product: parent._id,
+    });
 
+    return variants.map((x) => x.toObject());
+  };
 
-    getResolvers = () => {
-        return {
-            Mutation: {
-                createProduct: this.createProduct,
-                createProductPosting: this.createProductPosting
+  createProductPosting = resolver(async (parent, args, context, info) => {
+    const { variant, seller, price } = args.input;
 
-            },
-            Product: {
-                variants: this._ProductVariant
-            }
-        };
+    const productPosting = await this.ProductPosting.create({
+      variant,
+      seller,
+      price,
+    });
+
+    const productPostingObject = await productPosting.toObject();
+
+    return {
+      message: "Product has been posted",
+      productPosting: productPostingObject,
+    };
+  });
+
+  ProductPosting_variant = async (parent, args, context) => {
+    const variant = await this.ProductVariant.findById(parent.variant);
+    return variant.toObject();
+  };
+
+  ProductVariant_product = async (parent, args, context) => {
+    const product = await this.Product.findById(parent.product);
+    if (!product) {
+      throw createHttpError(404, "Product not found.");
     }
 
+    return product.toObject();
+  };
 
-
+  getResolvers = () => {
+    return {
+      Mutation: {
+        createProduct: this.createProduct,
+        createProductPosting: this.createProductPosting,
+      },
+      Product: {
+        variants: this.Product_variants,
+      },
+      ProductVariant: {
+        product: this.ProductVariant_product,
+      },
+      ProductPosting: {
+        variant: this.ProductPosting_variant,
+      },
+    };
+  };
 }
-
-
