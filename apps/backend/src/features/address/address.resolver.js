a3e48ff63697baebc27851341ec5691e86218e19
proghax333 <skillzworldtech@gmail.com>
2025-05-19 21:10:00 +0530

    feat: add auth check and addresses resolver

--- apps/backend/src/features/address/address.resolver.js
+++ apps/backend/src/features/address/address.resolver.js
@@ -17,9 +17,33 @@ export class AddressResolver {
     return ["Address", "Profile", "AddressDataLoader"];
   }
 
+  addresses = async (parent, args, context) => {
+    const user = context.user;
+    if (!user) {
+      throw createHttpError(401, "Not logged in.");
+    }
+
+    const profiles = await this.Profile.find({
+      user: context.user,
+    });
+    const profileIds = profiles.map((profile) => profile._id);
+
+    const pipeline = this.Address.aggregate().match({
+      profile: {
+        $in: profileIds,
+      },
+    });
+
+    const page = await paginate(pipeline, {
+      ...args,
+      all: true,
+    });
+
+    return page;
+  };
+
   createAddress = resolver(async (parent, args, context) => {
     const user = context.user;
-
     if (!user) {
       throw createHttpError(401, "Not logged in.");
     }
@@ -47,37 +71,82 @@ export class AddressResolver {
   });
 
   updateAddress = resolver(async (parent, args, context) => {
+    const user = context.user;
+    if (!user) {
+      throw createHttpError(401, "Not logged in.");
+    }
+
     const { id } = args;
 
     const address = await this.Address.findById(id);
-
     if (!address) {
       throw createHttpError(404, "Address not found.");
+    }
+
+    const profile = await this.Profile.findById(address.profile);
+    if (!profile) {
+      throw createHttpError(404, "Address not found.");
+    }
+
+    // If not the user's profile.
+    if (profile.user.toString() !== user._id.toString()) {
+      throw createHttpError(404, "Address not found.");
+    }
+
+    const result = await this.Address.findOneAndUpdate(id, args.input, {
+      new: true,
+    });
+
+    if (!result) {
+      throw createHttpError(500, "Could not update the address.");
     }
 
     return {
       message: "Address updated.",
-      address,
+      address: result,
     };
   });
 
   deleteAddress = resolver(async (parent, args, context) => {
+    const user = context.user;
+    if (!user) {
+      throw createHttpError(401, "Not logged in.");
+    }
+
     const { id } = args;
 
-    const address = await this.Address.findByIdAndDelete(id);
-
+    const address = await this.Address.findById(id);
     if (!address) {
       throw createHttpError(404, "Address not found.");
     }
 
+    const profile = await this.Profile.findById(address.profile);
+    if (!profile) {
+      throw createHttpError(404, "Address not found.");
+    }
+
+    // If not the user's profile.
+    if (profile.user.toString() !== user._id.toString()) {
+      throw createHttpError(404, "Address not found.");
+    }
+
+    const result = await this.Address.findOneAndDelete(id);
+
+    if (!result) {
+      throw createHttpError(500, "Could not delete the address.");
+    }
+
     return {
       message: "Address deleted.",
+      address: result,
     };
   });
 
   getResolvers() {
     return {
-      Query: {},
+      Query: {
+        addresses: this.addresses,
+      },
       Mutation: {
         createAddress: this.createAddress,
         updateAddress: this.updateAddress,
